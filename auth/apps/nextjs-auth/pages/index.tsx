59404f2fccf2edad88a989f64e0c1819cdbfdb97
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-03-25 15:43:02 +0500

    Set-up tailwindcss for nextjs-auth

--- auth/apps/nextjs-auth/pages/index.tsx
+++ auth/apps/nextjs-auth/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="flex w-full text-4xl">Web</h1>
       <Button />
     </div>
   );
