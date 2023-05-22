3a0a8350e82f5fcec8aa66d9f057a4941656bed9
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-05-23 04:31:29 +0500

    learn js converToJSONString2

--- solos/js/convertToJSONString.ts
+++ solos/js/convertToJSONString.ts
@@ -1,3 +1,4 @@
+// first version
 function jsonStringify(object: any): string {
   if (typeof object === 'string') {
     return `"${object}"`
@@ -14,7 +15,6 @@ function jsonStringify(object: any): string {
   }
   if (typeof object === 'object') {
     if (object === null) return 'null'
-    const keys = Object.keys(object)
     let sum: string[] = [];
     for (const key in object) {
       sum.push('"' + key + '":' + jsonStringify(object[key]))
@@ -23,3 +23,29 @@ function jsonStringify(object: any): string {
   }
   return ''
 };
+// second version
+function jsonStringify2(object: any): string {
+  switch (typeof object) {
+    case 'object':
+      if (object === null) return 'null';
+      if (Array.isArray(object)) {
+        let sum: string[] = [];
+        for (const item of object) {
+          sum.push(jsonStringify(item))
+        }
+        return `[${sum.join(',')}]`
+      }
+      let sum: string[] = [];
+      for (const key in object) {
+        sum.push('"' + key + '":' + jsonStringify(object[key]))
+      }
+      return `{${sum.join(',')}}`
+    case 'number':
+    case 'boolean':
+      return `${object}`
+    case 'string':
+      return `"${object}"`
+    default:
+      return ''
+  }
+}
