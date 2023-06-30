c43f7e248902752c1e4730f66a42854905b6e5a7
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-06-30 18:05:10 +0500

    add normalization and promises

--- docs/redux/redux-fundamentals-example-app/src/exampleAddons/middleware.js
+++ docs/redux/redux-fundamentals-example-app/src/exampleAddons/middleware.js
@@ -53,3 +53,15 @@ export const asyncFunctionMiddleware = storeAPI => next => action => {
 
   return next(action)
 }
+
+export const reduxThunkMiddleware = storeAPI => next => action => {
+  // If the "action" is actually a function instead...
+  if (typeof action === 'function') {
+    // then call the function and pass `dispatch` and `getState` as arguments
+    // Also, return whatever the thunk function returns
+    return action(storeAPI.dispatch, storeAPI.getState)
+  }
+
+  // Otherwise, it's a normal action - send it onwards
+  return next(action)
+}
