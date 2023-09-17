3b100ac09b12c8751c7fbba935f1ee0fc365d02c
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-09-17 20:44:38 +0500

    update rps

--- solos/py/arcade.py
+++ solos/py/arcade.py
@@ -2,51 +2,52 @@ import sys
 from guess_number import guess_number
 from rps import rps
 
-def play_game(name='ReadyPlayerOne'):
-    welcome_back=False
+
+def play_game(name="ReadyPlayerOne"):
+    welcome_back = False
 
     while True:
-        if welcome_back==True:
-            print(f'\n{name}, welcome back to the Arcade menu.')
-        
-        player_choice=input(
+        if welcome_back == True:
+            print(f"\n{name}, welcome back to the Arcade menu.")
+
+        player_choice = input(
             '\nPlease choose a game: \n1. Rock Paper Scissors \n2. Guess my number\n\n Or press "x" to exit.'
         )
 
-        welcome_back=True
+        welcome_back = True
 
-        if player_choice not in ['1','2','x']:
-            print(f'\n{name}, please enter 1, 2, or x.')
+        if player_choice not in ["1", "2", "x"]:
+            print(f"\n{name}, please enter 1, 2, or x.")
             return play_game(name)
-        
-        if player_choice == '1':
-            play_rps=rps(name)
-            play_rps() 
-        elif player_choice=='2':
-            play_gn=guess_number(name)
+
+        if player_choice == "1":
+            play_rps = rps(name)
+            play_rps()
+        elif player_choice == "2":
+            play_gn = guess_number(name)
             play_gn()
         else:
-            print('\n See you again!👋')
-            sys.exit(f'\n Bye {name}!')
+            print("\n See you again!👋")
+            sys.exit(f"\n Bye {name}!")
 
-if __name__=='__main__':
 
+if __name__ == "__main__":
     import argparse
 
-	
     parser = argparse.ArgumentParser(
-		description='Provides a personalized game experience.'
-	)
+        description="Provides a personalized game experience."
+    )
 
     parser.add_argument(
-		'-n','--name',metavar='name',
-		required=True, help='The name of the person playing the game.'
-	)
+        "-n",
+        "--name",
+        metavar="name",
+        required=True,
+        help="The name of the person playing the game.",
+    )
 
-	
-    args =parser.parse_args()
+    args = parser.parse_args()
 
-    print(f'\n{args.name}, welcome to the Arcade! 😎')
-	
+    print(f"\n{args.name}, welcome to the Arcade! 😎")
+
     play_game(args.name)
-    
--- solos/py/rps.py
+++ solos/py/rps.py
@@ -1,86 +1,99 @@
-# TODO rewrite the code
-# import math
 import sys
 import random
 from enum import Enum
 
-player=0
-computer=0
-draw=0
 
-def rps():
-	global player
-	global computer
-	global draw
+def rps(name="ReadyPlayerOne"):
+    player = 0
+    computer = 0
+    draw = 0
+    numbers_played = 0
 
-	class RPS(Enum):
-		Rock =1
-		PendingDeprecationWarningaper=2
-		Scissors=3
+    def play_rps():
+        class RPS(Enum):
+            Rock = 1
+            Paper = 2
+            Scissors = 3
 
-	print('\nPlease enter a number:')
+        print("\nPlease enter a number:")
 
-	number = input('\n numbers: \n1. Rock \n2. Paper \n3. Scissors \n\n')
-	number=int(number)
+        number = input("\n numbers: \n1. Rock \n2. Paper \n3. Scissors \n\n")
+        number = int(number)
 
-	#Recall def if wrong number
-	if number<1 and number>3:
-		print('\n Wrong number')
-		# guess_number()
-	
-	guess = random.choice('123')
-	guess=int(guess)
+        # Recall def if wrong number
+        if number < 1 and number > 3:
+            print("\n Wrong number")
+            return play_rps()
 
-	print(f'\n I guessed {guess} {RPS(guess)}!')
+        guess = random.choice("123")
+        guess = int(guess)
 
-	if (number==1 and guess==3) or (number==2 and guess==1) or (number==3 and guess==2):
-		print('\n You win!')
-		player+=1
-	elif number==guess:
-		print('\n Draw!')
-		draw+=1
-	else:
-		print('\n I win!')
-		computer+=1
+        print(f"\n You chose {str(RPS(number)).replace('RPS.', '').title()}!")
+        print(f"\n I guessed {str(RPS(guess)).replace('RPS.', '').title()}!")
 
-	
+        def find_winner(number, guess):
+            nonlocal player
+            nonlocal computer
+            nonlocal draw
+            if number == guess:
+                draw += 1
+                return "\n Draw!"
+            elif (
+                (number == 1 and guess == 3)
+                or (number == 2 and guess == 1)
+                or (number == 3 and guess == 2)
+            ):
+                player += 1
+                return "\n You win!"
 
-	print(f'\n Your wins: {player}')
-	print(f'\n My wins: {computer}')
-	print(f'\n Draws: {draw}')
+            else:
+                computer += 1
+                return "\n I win!"
+
+        print(find_winner(number, guess))
+
+        nonlocal numbers_played
+        numbers_played += 1
+
+        print(f"\n Games played: {numbers_played}")
+        print(f"\n Your wins: {player}")
+        print(f"\n My wins: {computer}")
+        print(f"\n Draws: {draw}")
+
+        print("\n Do you want to play again?")
+
+        def exit_loop():
+            ans = input("\n Y for yes,\n N for no and to quit?\n")
+            if ans.lower() == "y":
+                rps()
+            elif ans.lower() == "n":
+                if __name__ == "__main__":
+                    sys.exit(f"Bye {name}! 👋")
+                else:
+                    return
+            else:
+                print("\n Wrong input enter again!")
+                exit_loop()
+
+        exit_loop()
+
+    return play_rps
 
 
-	print('\n Do you want to play again?')
-	def exit_loop():
-		ans = input('\n Y for yes,\n N for no and to quit?\n')
-		if ans.lower()=='y':
-			rps()
-		elif ans.lower()=='n':
-			sys.exit()
-		else:
-			print('\n Wrong input enter again!')
-			exit_loop()
-	
-	exit_loop()
+if __name__ == "__main__":
+    import argparse
 
+    parser = argparse.ArgumentParser(description="Provide personalized experience.")
 
+    parser.add_argument(
+        "-n",
+        "--name",
+        metavar="name",
+        required=True,
+        help="The name of the person playing the game.",
+    )
 
+    args = parser.parse_args()
 
-
-
-if __name__ == '__main__':
-	import argparse
-
-	parser = argparse.ArgumentParser(
-		description='Provide personalized experience.'
-	)
-
-	parser.add_argument(
-		'-n','--name',metavar='name',
-		required=True, help='The name of the person playing the game.'
-	)
-
-	args =parser.parse_args()
-
-	my_rps = rps(args.name)
-	my_rps()
+    my_rps = rps(args.name)
+    my_rps()
