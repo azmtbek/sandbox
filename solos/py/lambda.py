b2b887f6c49e9d534dacaf3ec5ad67d8fb8cc74f
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-09-19 22:24:00 +0500

    some changes

--- solos/py/lambda.py
+++ solos/py/lambda.py
@@ -1,11 +1,15 @@
 from functools import reduce
-squared = lambda num: num*num
 
-addTwo = lambda num:num+2
+squared = lambda num: num * num
+
+addTwo = lambda num: num + 2
 
 
-names = ['John','Doe','Lambda']
+names = ["John", "Doe", "Lambda"]
 
-char_count= reduce(lambda acc, curr: acc+len(curr),names,0)
+char_count = reduce(lambda acc, curr: acc + len(curr), names, 0)
 
 print(char_count)
+
+
+print(addTwo(40))
